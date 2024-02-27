4d1f1582011641cf5d48610a7b9532fa2fadd562
rahulkmilan <125357153+rahulkmilan@users.noreply.github.com>
2024-02-28 02:04:03 +0530

    Update settings.py

--- carbook/settings.py
+++ carbook/settings.py
@@ -119,8 +119,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rahulkmilan92@gmail.com'
-EMAIL_HOST_PASSWORD = 'geyd rwqb dmdf trko'
+EMAIL_HOST_USER = 'your_mail_id@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 EMAIL_USE_SSL = False
 
